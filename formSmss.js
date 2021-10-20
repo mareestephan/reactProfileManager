5e8d0dc5997d3ee2dae7d37675aa8919aa055558
Stephan Maree <35425202+mareestephan@users.noreply.github.com>
2021-10-20 16:26:31 +0200

    Update formSmss.js

--- formSmss.js
+++ formSmss.js
@@ -1,5 +1,14 @@
-document.getElementsByClassName("normal-state").addEventListener("submit", myFunction);
+// document.getElementsByTagsName("normal-state").addEventListener("submit", myFunction);
 
-function myFunction() {
+// function myFunction() {
+//   console.log("appels");
+// }
+
+var inputs = document.getElementsByTagName(‘button’);
+
+for(var i=0; i<inputs.length; i++){
+if(inputs[i].getAttribute(‘type’)==‘submit’){
   console.log("appels");
+
+}
 }
